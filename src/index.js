2e27d6f3c7b0ca3c8ba1b9c07b02e4c1446952ea
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-30 18:18:41 -0500

    added logger and redux-saga

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
+import createSagaMiddleware from 'redux-saga';
+import{ put, takeEvery } from 'redux-saga/effects';
+import logger from 'redux-logger';
 import App from './App';
 
 // this startingPlantArray should eventually be removed
@@ -11,6 +13,12 @@ const startingPlantArray = [
   { id: 2, name: 'Tulip' },
   { id: 3, name: 'Oak' }
 ];
+
+function* watcherSaga() {
+
+}
+
+const sagaMiddleware = createSagaMiddleware();
 
 const plantList = (state = startingPlantArray, action) => {
   switch (action.type) {
@@ -23,6 +31,9 @@ const plantList = (state = startingPlantArray, action) => {
 
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(logger, sagaMiddleware)
 );
+
+sagaMiddleware.run(watcherSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
